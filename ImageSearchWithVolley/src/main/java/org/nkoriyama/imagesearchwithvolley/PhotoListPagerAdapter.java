2d1a7c7532f52d3b86b951b49639a505ecff5be5
Naohiro KORIYAMA <nkoriyama@gmail.com>
2014-04-07 00:59:53 +0900

    Use Preconditions for checking element index.

--- ImageSearchWithVolley/src/main/java/org/nkoriyama/imagesearchwithvolley/PhotoListPagerAdapter.java
+++ ImageSearchWithVolley/src/main/java/org/nkoriyama/imagesearchwithvolley/PhotoListPagerAdapter.java
@@ -5,6 +5,8 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.support.v13.app.FragmentPagerAdapter;
 
+import com.google.common.base.Preconditions;
+
 import java.util.List;
 
 public class PhotoListPagerAdapter extends FragmentPagerAdapter {
@@ -21,6 +23,7 @@ public class PhotoListPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int i) {
+        Preconditions.checkElementIndex(i, mFragmentClassList.size());
         if (mFragmentClassList.get(i) == FlickrPhotoListFragment.class) {
             return FlickrPhotoListFragment.newInstance(mQuery);
         }
@@ -40,6 +43,7 @@ public class PhotoListPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public CharSequence getPageTitle(int position) {
+        Preconditions.checkElementIndex(position, mFragmentClassList.size());
         final String fragmentName = mFragmentClassList.get(position).getSimpleName();
         return fragmentName.substring(0, fragmentName.indexOf("PhotoListFragment"));
     }
