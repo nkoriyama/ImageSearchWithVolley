e76f6e650e73033e25a5e0c7a310c7941c08d810
Naohiro KORIYAMA <nkoriyama@gmail.com>
2014-07-04 05:32:40 +0900

    Using okhttp-urlconnection.

--- ImageSearchWithVolley/src/main/java/org/nkoriyama/imagesearchwithvolley/OkHttpStack.java
+++ ImageSearchWithVolley/src/main/java/org/nkoriyama/imagesearchwithvolley/OkHttpStack.java
@@ -1,35 +1,18 @@
 package org.nkoriyama.imagesearchwithvolley;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.toolbox.HttpStack;
-import com.squareup.okhttp.Call;
-import com.squareup.okhttp.Headers;
-import com.squareup.okhttp.MediaType;
+import com.android.volley.toolbox.HurlStack;
 import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Protocol;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
-import com.squareup.okhttp.ResponseBody;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.ProtocolVersion;
-import org.apache.http.StatusLine;
-import org.apache.http.entity.BasicHttpEntity;
-import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicHttpResponse;
-import org.apache.http.message.BasicStatusLine;
+import com.squareup.okhttp.OkUrlFactory;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * An {@link com.android.volley.toolbox.HttpStack HttpStack} implementation which
  * uses OkHttp as its transport.
  */
-public class OkHttpStack implements HttpStack {
+public class OkHttpStack extends HurlStack {
     private final OkHttpClient client;
 
     public OkHttpStack() {
@@ -43,132 +26,7 @@ public class OkHttpStack implements HttpStack {
         this.client = client;
     }
 
-    @Override
-    public HttpResponse performRequest(Request<?> request, Map<String, String> additionalHeaders) throws IOException, AuthFailureError {
-        final String url = request.getUrl();
-        final com.squareup.okhttp.Request.Builder okHttpRequestBuilder = new com.squareup.okhttp.Request.Builder();
-
-        int timeoutMs = request.getTimeoutMs();
-        client.setConnectTimeout(timeoutMs, TimeUnit.MILLISECONDS);// OkHttp-UrlConnection -> setConnectionTimeout(long)
-        client.setReadTimeout(timeoutMs, TimeUnit.MILLISECONDS);// OkHttp-UrlConnection -> setReadTimeout(long)
-
-        final Map<String, String> headers = request.getHeaders();
-        for (final String name : headers.keySet()) {
-            okHttpRequestBuilder.addHeader(name, headers.get(name));
-        }
-        for (final String name : additionalHeaders.keySet()) {
-            okHttpRequestBuilder.addHeader(name, additionalHeaders.get(name));
-        }
-
-        okHttpRequestBuilder.url(url);
-        setConnectionParametersForRequest(okHttpRequestBuilder, request);
-
-        final com.squareup.okhttp.Request okHttpRequest = okHttpRequestBuilder.build();
-        final Call okHttpCall = client.newCall(okHttpRequest);
-        final Response okHttpResponse = okHttpCall.execute();
-
-        final int responseCode = okHttpResponse.code();
-        if (responseCode == -1) {
-            // -1 is returned by getResponseCode() if the response code could not be retrieved.
-            // Signal to the caller that something was wrong with the connection.
-            throw new IOException("Could not retrieve response code from HttpUrlConnection.");
-        }
-
-        final Protocol protocol = okHttpResponse.protocol();
-
-        StatusLine responseStatus = new BasicStatusLine(parseProtocol(protocol), okHttpResponse.code(), okHttpResponse.message());
-        BasicHttpResponse response = new BasicHttpResponse(responseStatus);
-        response.setEntity(entityFromOkHttpResponse(okHttpResponse));
-
-        final Headers responseHeaders = okHttpResponse.headers();
-        for (int i = 0, len = responseHeaders.size(); i < len; i++) {
-            final String name = responseHeaders.name(i), value = responseHeaders.value(i);
-            if (name != null) {
-                response.addHeader(new BasicHeader(name, value));
-            }
-        }
-
-        return response;
-    }
-
-    private static HttpEntity entityFromOkHttpResponse(Response r) {
-        final BasicHttpEntity entity = new BasicHttpEntity();
-        final ResponseBody body = r.body();
-
-        entity.setContent(body.byteStream());
-        entity.setContentLength(body.contentLength());
-        entity.setContentEncoding(r.header("Content-Encoding"));// Default action for UrlConnection
-
-        entity.setContentType(body.contentType().type());
-
-        return entity;
-    }
-
-    @SuppressWarnings("deprecation")
-    private static void setConnectionParametersForRequest(com.squareup.okhttp.Request.Builder builder, Request<?> request) throws IOException, AuthFailureError {
-        switch (request.getMethod()) {
-            case Request.Method.DEPRECATED_GET_OR_POST:
-                // This is the deprecated way that needs to be handled for backwards compatibility.
-                // If the request's post body is null, then the assumption is that the request is
-                // GET.  Otherwise, it is assumed that the request is a POST.
-                byte[] postBody = request.getPostBody();
-                if (postBody != null) {
-                    // Prepare output. There is no need to set Content-Length explicitly,
-                    // since this is handled by HttpURLConnection using the size of the prepared
-                    // output stream.
-                    builder.post(RequestBody.create(MediaType.parse(request.getPostBodyContentType()), postBody));
-                }
-                break;
-            case Request.Method.GET:
-                // Not necessary to set the request method because connection defaults to GET but
-                // being explicit here.
-                builder.get();
-                break;
-            case Request.Method.DELETE:
-                builder.delete();
-                break;
-            case Request.Method.POST:
-                builder.post(createRequestBody(request));
-                break;
-            case Request.Method.PUT:
-                builder.put(createRequestBody(request));
-                break;
-            case Request.Method.HEAD:
-                builder.head();
-                break;
-            case Request.Method.OPTIONS:
-                builder.method("OPTIONS", null);
-                break;
-            case Request.Method.TRACE:
-                builder.method("TRACE", null);
-                break;
-            case Request.Method.PATCH:
-                builder.patch(createRequestBody(request));
-                break;
-            default:
-                throw new IllegalStateException("Unknown method type.");
-        }
-    }
-
-    private static final ProtocolVersion parseProtocol(final Protocol p) {
-        switch (p) {
-            case HTTP_1_0:
-                return new ProtocolVersion("HTTP", 1, 0);
-            case HTTP_1_1:
-                return new ProtocolVersion("HTTP", 1, 1);
-            case SPDY_3:
-                return new ProtocolVersion("SPDY", 3, 1);//TODO Check volley implementation because I'm not sure if this is okay.
-            case HTTP_2:
-                return new ProtocolVersion("HTTP", 2, 0);
-        }
-
-        throw new IllegalAccessError("Unkwown protocol");
-    }
-
-    private static final RequestBody createRequestBody(Request r) throws AuthFailureError {
-        final byte[] body = r.getBody();
-        if (body == null) return null;
-
-        return RequestBody.create(MediaType.parse(r.getBodyContentType()), body);
+    @Override protected HttpURLConnection createConnection(URL url) throws IOException {
+        return new OkUrlFactory(client).open(url);
     }
 }
