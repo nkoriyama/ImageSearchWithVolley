8a08cc299c9e588c7def06c1bb702a9ef55ea4ee
Naohiro KORIYAMA <nkoriyama@gmail.com>
2014-10-26 11:09:16 +0900

    Use reflection and keep newInstance method name from ProGuard.

--- ImageSearchWithVolley/src/main/java/org/nkoriyama/imagesearchwithvolley/PhotoListPagerItem.java
+++ ImageSearchWithVolley/src/main/java/org/nkoriyama/imagesearchwithvolley/PhotoListPagerItem.java
@@ -2,34 +2,38 @@ package org.nkoriyama.imagesearchwithvolley;
 
 import android.app.Fragment;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class PhotoListPagerItem {
-    private final Class<? extends PhotoListFragment> mFragmentClass;
+    private final Class<? extends PhotoListFragment> mPhotoListFragmentClass;
     private final int mIndicatorColor;
     private final int mDividerColor;
 
 
     PhotoListPagerItem(Class<? extends PhotoListFragment> fragmentClass,
                        int indicatorColor, int dividerColor) {
-        mFragmentClass = fragmentClass;
+        mPhotoListFragmentClass = fragmentClass;
         mIndicatorColor = indicatorColor;
         mDividerColor = dividerColor;
     }
 
     Fragment createFragment(String query) {
-        if (mFragmentClass == FlickrPhotoListFragment.class) {
-            return FlickrPhotoListFragment.newInstance(query);
-        }
-        if (mFragmentClass == BingPhotoListFragment.class) {
-            return BingPhotoListFragment.newInstance(query);
-        }
-        if (mFragmentClass == AmazonPhotoListFragment.class) {
-            return AmazonPhotoListFragment.newInstance(query);
+        try {
+            Method method = mPhotoListFragmentClass.getMethod("newInstance", String.class);
+            return (PhotoListFragment)method.invoke(null, query);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
         return null;
     }
 
     CharSequence getTitle() {
-        final String fragmentName =  mFragmentClass.getSimpleName();
+        final String fragmentName =  mPhotoListFragmentClass.getSimpleName();
         return fragmentName.substring(0, fragmentName.indexOf("PhotoListFragment"));
     }
 
