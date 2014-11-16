08185a116eb7db38cabb1250cf9ba30eaf38064b
Naohiro KORIYAMA <nkoriyama@gmail.com>
2014-11-17 03:02:56 +0900

    support OAuth request

---
+++ app/src/main/java/org/nkoriyama/imagesearchwithvolley/OAuthHurlStack.java
@@ -0,0 +1,34 @@
+package org.nkoriyama.imagesearchwithvolley;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+
+public class OAuthHurlStack extends OkHttpStack {
+
+    private final OAuthConsumer consumer;
+
+    public OAuthHurlStack(OAuthConsumer consumer) {
+        this.consumer = consumer;
+    }
+
+    @Override
+    protected HttpURLConnection createConnection(URL url) throws IOException {
+        HttpURLConnection connection = super.createConnection(url);
+        try {
+            this.consumer.sign(connection);
+        } catch (OAuthMessageSignerException e) {
+            e.printStackTrace();
+        } catch (OAuthExpectationFailedException e) {
+            e.printStackTrace();
+        } catch (OAuthCommunicationException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
--- app/src/main/java/org/nkoriyama/imagesearchwithvolley/RequestQueueHelper.java
+++ app/src/main/java/org/nkoriyama/imagesearchwithvolley/RequestQueueHelper.java
@@ -5,9 +5,15 @@ import android.content.Context;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import oauth.signpost.OAuthConsumer;
+
 public class RequestQueueHelper {
     private static Context sContext;
     private static RequestQueue sRequestQueue;
+    private static Map<OAuthConsumer, RequestQueue> sOAuthConsumerRequestQueueMap = new HashMap<OAuthConsumer, RequestQueue>();
 
     public static void init(Context context) {
         sContext = context;
@@ -18,5 +24,14 @@ public class RequestQueueHelper {
             sRequestQueue = Volley.newRequestQueue(sContext, new OkHttpStack());
         }
         return sRequestQueue;
+    }
+
+    public static synchronized RequestQueue getRequestQueue(OAuthConsumer consumer) {
+        if (!sOAuthConsumerRequestQueueMap.containsKey(consumer)) {
+            sOAuthConsumerRequestQueueMap.put(
+                    consumer,
+                    Volley.newRequestQueue(sContext, new OAuthHurlStack(consumer)));
+        }
+        return sOAuthConsumerRequestQueueMap.get(consumer);
     }
 }
