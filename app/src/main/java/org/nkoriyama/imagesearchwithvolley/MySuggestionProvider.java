a0dc066b75b42a48a9ef8034a1dd2b46b598cc9f
Naohiro KORIYAMA <nkoriyama@gmail.com>
2015-01-04 21:07:02 +0900

    use different authorities for release/debug

--- app/src/main/java/org/nkoriyama/imagesearchwithvolley/MySuggestionProvider.java
+++ app/src/main/java/org/nkoriyama/imagesearchwithvolley/MySuggestionProvider.java
@@ -3,7 +3,7 @@ package org.nkoriyama.imagesearchwithvolley;
 import android.content.SearchRecentSuggestionsProvider;
 
 public class MySuggestionProvider extends SearchRecentSuggestionsProvider {
-    public final static String AUTHORITY = "org.nkoriyama.imagesearchwithvolley.MySuggestionProvider";
+    public static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".provider";
     public final static int MODE = DATABASE_MODE_QUERIES;
 
     public MySuggestionProvider() {
