840236b8bba4435f7f02bf202a762d6f14d7e8a6
Naohiro KORIYAMA <nkoriyama@gmail.com>
2015-06-10 10:11:26 +0900

    multidex support crashlyitics to fabric

--- app/src/main/java/org/nkoriyama/imagesearchwithvolley/ImageSearchWithVolley.java
+++ app/src/main/java/org/nkoriyama/imagesearchwithvolley/ImageSearchWithVolley.java
@@ -1,10 +1,16 @@
 package org.nkoriyama.imagesearchwithvolley;
 
-import android.app.Application;
+
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import com.crashlytics.android.Crashlytics;
 
-public class ImageSearchWithVolley extends Application {
+import io.fabric.sdk.android.Fabric;
+
+
+public class ImageSearchWithVolley extends MultiDexApplication {
 
     public void onCreate() {
         super.onCreate();
@@ -12,7 +18,17 @@ public class ImageSearchWithVolley extends Application {
         RequestQueueHelper.init(getApplicationContext());
 
         if (BuildConfig.USE_CRASHLYTICS) {
-            Crashlytics.start(this);
+            Fabric.with(this, new Crashlytics());
         }
+
+
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 }
+
+
